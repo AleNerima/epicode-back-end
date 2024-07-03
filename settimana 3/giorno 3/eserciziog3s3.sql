bd67491707bd3d4e22ac59f1114d5c0426752452
AleNerima <ale95polioni@gmail.com>
2024-07-03 14:32:06 +0200

    esercizio giorno3 s3

---
+++ settimana 3/giorno 3/eserciziog3s3.sql
@@ -0,0 +1,43 @@
+﻿select COUNT(*) from Orders
+
+select COUNT(*) from Employees
+
+select COUNT(*) from Employees
+where City='London'
+
+select AVG(Freight) 
+From Orders
+
+select AVG(Freight) 
+From Orders
+Where CustomerID='BOTTM'
+
+SELECT CustomerID, AVG(Freight)
+FROM Orders
+Group by CustomerID
+
+SELECT COUNT(*) , City
+FROM Employees 
+Group by City
+
+SELECT OrderId, SUM((UnitPrice*Quantity)) As TotaleRiga
+From [Order Details]
+Group by OrderID
+
+SELECT OrderId, SUM((UnitPrice*Quantity)) As TotaleRiga
+From [Order Details]
+Group by OrderID
+Having OrderID=10248
+
+Select Count(*), CategoryID
+From Products
+Group by CategoryID
+
+Select Count(*), ShipCountry
+From Orders
+Group by ShipCountry
+
+Select AVG(Freight), ShipCountry
+From Orders
+Group by ShipCountry
+
